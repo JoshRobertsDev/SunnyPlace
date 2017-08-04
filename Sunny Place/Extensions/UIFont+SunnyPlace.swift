9c67b8fe8896de2e396f2c07c84dc1a8a2f8d3f0
Josh Roberts <josh.r@brandtix.com>
2017-08-04 11:45:37 +0100

    - Added some theme fonts

---
+++ Sunny Place/Extensions/UIFont+SunnyPlace.swift
@@ -0,0 +1,20 @@
+//
+//  UIFont+SunnyPlace.swift
+//  Sunny Place
+//
+//  Created by Josh Roberts on 03/08/2017.
+//  Copyright © 2017 Josh Roberts. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    class var title: UIFont {
+        return .systemFont(ofSize: 17, weight: .light)
+    }
+    
+    class var value: UIFont {
+        return .systemFont(ofSize: 22, weight: .medium)
+    }
+}
